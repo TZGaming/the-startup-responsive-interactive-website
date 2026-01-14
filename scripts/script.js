055a4e99d1b6166def618dcf76a7d310bfe904fb
TZGaming <tomzuidervaart@gmail.com>
2026-01-14 12:39:06 +0100

    Meer interacties toegevoegd, #9

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,34 @@ let hiddenfield = document.querySelector('.hidden-field');
 let convertBtn = document.querySelector('.convert-btn');
 
 convertBtn.addEventListener('click', function () {
-    inputfield.classList.add('input-moving');
+    if (inputfield.value.trim() === '') return;
 
+    inputfield.classList.add('input-moving');
     hiddenfield.classList.replace('hidden-field', 'unhidden-field');
+});
+
+inputfield.addEventListener('animationend', function () {
+    inputfield.classList.remove('input-moving');
+    hiddenfield.classList.replace('unhidden-field', 'hidden-field');
+
+    inputfield.value = '';
+});
+
+
+let sideBar = document.querySelector('.side-bar');
+let hamburger = document.querySelector('.hamburger-icon');
+
+hamburger.addEventListener('click', function () {
+  sideBar.classList.toggle('open');
+  hamburger.classList.toggle('open');
+});
+
+let primaryButton = document.querySelector('.convert-btn');
+
+primaryButton.addEventListener('click', function () {
+  primaryButton.classList.toggle('click');
 
     setTimeout(() => {
-        inputfield.classList.remove('input-moving');
-
-        hiddenfield.classList.replace('unhidden-field', 'hidden-field');
-    }, 750);
+        primaryButton.classList.remove('click');
+    }, 250);
 });
